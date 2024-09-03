451f7ca58e6af9fdc522714ea25285e1d21df6ae
Andreas Giereck Kristiansen <giereck@hotmail.com>
2024-09-03 21:57:07 +0200

    Can now ben-decode a number.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,5 @@
 use clap::{Args, Parser, Subcommand};
 
-// Available if you need it!
-// use serde_bencode
-
-#[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_ascii_digit() {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        serde_json::Value::String(string.to_string())
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
-    }
-}
-
 #[derive(Parser)]
 #[clap(version, about, long_about = None)]
 #[command(propagate_version = true)]
@@ -43,8 +25,25 @@ fn main() {
     match cli.commands {
         Commands::Decode(args) => {
             let encoded_value = args.value;
-            let decoded_value: String = serde_bencode::from_str(&encoded_value).unwrap();
+            let decoded_value = decode_bencoded_value(&encoded_value);
             println!("{}", decoded_value);
         }
+    }
+}
+
+#[allow(dead_code)]
+fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
+    // If encoded_value starts with a digit, it's a number
+    let first_char = encoded_value.chars().next().unwrap();
+    // Example: "5:hello" -> "hello"
+    if first_char.is_ascii_digit() {
+        let decoded: String = serde_bencode::from_str(encoded_value).unwrap();
+        serde_json::Value::String(decoded)
+    // Example: i52e -> 52
+    } else if first_char == 'i' {
+        let decoded: i64 = serde_bencode::from_str(encoded_value).unwrap();
+        serde_json::Value::Number(serde_json::Number::from(decoded))
+    } else {
+        panic!("Unhandled encoded value: {}", encoded_value)
     }
 }
